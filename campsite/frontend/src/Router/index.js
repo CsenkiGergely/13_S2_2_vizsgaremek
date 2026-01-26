57efc1965ee3d00b872e777fa34693b7eec64894
Csenki Gergely <118981063+CsenkiGergely@users.noreply.github.com>
2026-01-26 16:56:21 +0100

    admin

--- campsite/frontend/src/Router/index.js
+++ campsite/frontend/src/Router/index.js
@@ -1,10 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import kereses from '../views/kereses.vue'
+import fizetes from '../views/fizetes.vue'
+
 
 const routes = [
   { path: '/', name: 'Home', component: Home },
-  { path: '/kereses', name: 'kereses', component: kereses }
+  { path: '/kereses', name: 'kereses', component: kereses },
+  { path: '/fizetes', name: 'fizetes', component: fizetes }
+
 ]
 
 const router = createRouter({
