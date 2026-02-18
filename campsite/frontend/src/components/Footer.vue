e35ddedf773dec45ca0a25106290d76bc983f17a
Senki Gergely <118981063+CsenkiGergely@users.noreply.github.com>
2026-02-18 12:12:59 +0100

    tulajdonos javítás

--- campsite/frontend/src/components/Footer.vue
+++ campsite/frontend/src/components/Footer.vue
@@ -27,14 +27,14 @@
             <router-link to="/kereses">
       <a href="kereses.vue">Keresés</a>
     </router-link>
-                <router-link to="/admin">
-     <a href="#">Admin</a>
+                <router-link to="/Tulajdonos">
+     <a href="#">Tulajdonos</a>
     </router-link>
             <router-link to="/foglalas">
      <a href="#">Foglalás</a>
     </router-link>
                 <router-link to="/fizetes">
-     <a href="#">fizetes</a>
+     <a href="#">fizetes0</a>
     </router-link>
   </div>
 
